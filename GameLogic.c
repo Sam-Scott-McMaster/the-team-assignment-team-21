49ff41646bd4cc4ce5e16ab8aaf3044f0cdd524a
Josh Engel <engelj4@mcmaster.ca>
2024-11-20 13:45:37 -0500

    addition_of_build.yml

--- GameLogic.c
+++ GameLogic.c
@@ -5,7 +5,8 @@
 #define HEIGHT 6
 #define WIDTH 7
 
-void freeArray (double **array, int R) {
+//frees a malloc array
+void freeArray (int **array, int R) {
     for (int i = 0; i < R; i++) {
         free(array[i]);
     }
@@ -13,15 +14,16 @@ void freeArray (double **array, int R) {
     return;
 }
 
+//creates an empty game board
 int **createGameBoard () {
-    double **game_board = (double **)malloc(HEIGHT* sizeof(double *));
+    int **game_board = (int **)malloc(HEIGHT* sizeof(int *));
     if (game_board == NULL) {
         fprintf(stderr, "Memory allocation failed for rows.\n");
         return NULL;
     }
 
     for (int i = 0; i < HEIGHT; i++) {
-        game_board[i] = (double *)malloc(WIDTH * sizeof(double));
+        game_board[i] = (int *)malloc(WIDTH * sizeof(int));
         if (game_board[i] == NULL) {
             fprintf(stderr, "Memory allocation failed for row %d.\n", i);
             return NULL;
@@ -36,15 +38,16 @@ int **createGameBoard () {
     return game_board;
 }
 
-int **updateGameBoard (int placement, int player, int **game_board, bool *hide_button) {
-    double **new_game_board = (double **)malloc(HEIGHT * sizeof(double *));
+//updates the gameboard after a button test as well as returning a side effect to hide a GUI button and the placement of the piece
+int **updateGameBoard (int placement, int player, int **game_board, int *hide_button, int *placement_height) {
+    int **new_game_board = (int **)malloc(HEIGHT * sizeof(int *));
     if (new_game_board == NULL) {
         fprintf(stderr, "Memory allocation failed for rows.\n");
         return NULL;
     }
 
     for (int i = 0; i < HEIGHT; i++) {
-        new_game_board[i] = (double *)malloc(WIDTH * sizeof(double));
+        new_game_board[i] = (int *)malloc(WIDTH * sizeof(int));
         if (new_game_board[i] == NULL) {
             fprintf(stderr, "Memory allocation failed for row %d.\n", i);
             return NULL;
@@ -63,18 +66,42 @@ int **updateGameBoard (int placement, int player, int **game_board, bool *hide_b
     for (int i = 0; i < HEIGHT; i++) {
         if (new_game_board[i][placement] == 0) {
             new_game_board[i][placement] = player;
-            check = 1;
+            *placement_height = i;
             break;
+        } else {
+            check += 1;
         }
     }
-    if (check == 1) {
-        hide_button = true;
+    if (check == 6) {
+        *hide_button = true;
     }
 
     return new_game_board;
 }
 
+//changes the current player
 void playerSelection (int *player) {
     *player = 3 - *player;
     return;
 }
+
+//test main function run
+/*
+void main () {
+    int placement_height = 0;
+    int player = 1;
+    int **gameboard = createGameBoard();
+    while (true) {
+        int placement;
+        printf("Player %d what row would you like to place it in: ", player);
+        scanf("%d", &placement);
+        gameboard = updateGameBoard(placement,player,gameboard,false,&placement_height);
+        playerSelection(&player);
+        for(int j=HEIGHT-1;j>-1; j--) {
+            for(int k=0;k<WIDTH; k++) {
+                printf("%d",gameboard[j][k]);
+            }
+            printf("\n");
+        }
+    }
+}*/
---
+++ makefile
@@ -0,0 +1,2 @@
+GameLogic: GameLogic.c
+	gcc -o GameLogic GameLogic.c
