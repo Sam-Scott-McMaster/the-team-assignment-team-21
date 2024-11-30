c5996de6a841ea0480919cc103742dcb79eb3720
sameerkhichi <khichis@mcmaster.ca>
2024-11-30 03:36:44 -0500

    Adjusting the help screen GUI to adhere to the CSS rules used for the other ones

--- help_screen.c
+++ help_screen.c
@@ -1,36 +1,102 @@
 #include <gtk/gtk.h>
+#include "help_screen.h"
+#include "game_board.h"
 
-//this function makes the game board itself, creating the window
+//function that starts the game from the help screen
+void begin_game(GtkWidget *widget, gpointer data) {
+    GtkWidget *help_window = GTK_WIDGET(data);
+    //closes the help screen window, then starts the game
+    gtk_widget_destroy(help_window); 
+    create_game_board(); 
+}
+
+//function that quits the game from the help screen
+void prequit_game(GtkWidget *widget, gpointer data) {
+    gtk_main_quit();
+}
+
+//loading the CSS rules
+void help_load_css(){
+    GtkCssProvider *provider = gtk_css_provider_new();
+    GdkDisplay *display = gdk_display_get_default();
+    GdkScreen *screen = gdk_display_get_default_screen(display);
+
+    //these are the same rules from the other GUI screens
+    gtk_css_provider_load_from_data(provider,
+        "button.blue-button {"
+        "    background: #4682B4;"
+        "    color: white;"
+        "    border: 2px solid #4682B4;"
+        "    border-radius: 10px;"
+        "    padding: 10px;"
+        "    box-shadow: none;"
+        "    background-image: none;"
+        "}"
+        "button.blue-button:hover {"
+        "    background: #236ead;"
+        "    border-color: #236ead;"
+        "    box-shadow: none;"
+        "}"
+        ".welcome-label {"
+        "    color: white;"
+        "    font-size: 20px;" //text size for the labels
+        "    margin: 10px;"    //adding some spacing
+        "}", -1, NULL);
+
+    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+    g_object_unref(provider);
+}
+
 void create_help_screen(){
-   
-    //creating a new window, requires new variables
     GtkWidget *window;
     GtkWidget *grid;
-    GtkWidget *label;
+    GtkWidget *label1, *label2, *label3;
+    GdkRGBA background_colour;
 
-    //creating the window using the init, but no arguments passed
+    gdk_rgba_parse(&background_colour, "#4682B4");
+
     gtk_init(NULL, NULL);
-   
-    //create game board window just like the main menu, same proccess
+
+    //loading CSS for this function
+    help_load_css();
+
+    //create main window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Connect Four");
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     gtk_window_maximize(GTK_WINDOW(window));
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_widget_override_background_color(window, GTK_STATE_FLAG_NORMAL, &background_colour);
 
-    //this is the grid that is used to organize the board
+    //creating a grid to format the labels
     grid = gtk_grid_new();
+    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(label), "<span font='40'>Connect Four in a row before your opponent\nDo This by connecting on a diagonal, vertical, or horizontal\nTo place a piece click on the corresponding collumns button</span>");
-    gtk_widget_set_halign(label, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(label, GTK_ALIGN_START);
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
+    //creating texts with labels
+    label1 = gtk_label_new("Welcome to Connect Four!");
+    label2 = gtk_label_new("Use the arrow keys to drop your disc.");
+    label3 = gtk_label_new("First to connect four discs wins!");
 
-    //showing the game board window
+    //the labels are made with CSS
+    GtkStyleContext *label1_context = gtk_widget_get_style_context(label1);
+    gtk_style_context_add_class(label1_context, "welcome-label");
+
+    GtkStyleContext *label2_context = gtk_widget_get_style_context(label2);
+    gtk_style_context_add_class(label2_context, "welcome-label");
+
+    GtkStyleContext *label3_context = gtk_widget_get_style_context(label3);
+    gtk_style_context_add_class(label3_context, "welcome-label");
+
+    //attatching the labels to the grid and formatting them correctly
+    gtk_grid_attach(GTK_GRID(grid), label1, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), label2, 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), label3, 0, 2, 1, 1);
+
     gtk_widget_show_all(window);
-
-    //running the gtk main loop
+   
     gtk_main();
+
+    return 0;
 }
