33c9c6d2384d954488af1d18e4ca606d370314e2
leep48 <leep48@mcmaster.ca>
2024-11-30 19:18:58 -0500

    Fixing conflicts in check_win.h

--- check_win.h
+++ check_win.h
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 bool check_vertical(int **game_board, int current_row, int current_col);
 bool check_horizontal(int **game_board, int current_row, int current_col);
 bool check_diagonal_descending(int **game_board, int current_row, int current_col);
 bool check_diagonal_ascending(int **game_board, int current_row, int current_col);
-=======
-bool check_vertical(int (*game_board)[], int current_row, int current_col);
->>>>>>> 517ea6b (Created a function to check for a win vertically)
-=======
-bool check_vertical(int **game_board, int current_row, int current_col);
-bool check_horizontal(int **game_board, int current_row, int current_col);
-bool check_diagonal_descending(int **game_board, int current_row, int current_col);
-bool check_diagonal_ascending(int **game_board, int current_row, int current_col);
->>>>>>> 4658b39 (Added test_win_check.yml to test win_check_utilities and upload a report)
