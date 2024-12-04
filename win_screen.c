7b338fce922dcf745f4f7dc8fe4f2e52ea4b2984
Josh Engel <engelj4@mcmaster.ca>
2024-12-04 13:59:12 -0500

    v2.0.2

--- win_screen.c
+++ win_screen.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <gtk/gtk.h>
 #include "game_board.h"
 #include "help_screen.h"
@@ -137,45 +136,4 @@ int create_win_screen(int player){
     gtk_main();
 
     return 0;
-=======
-#include <gtk/gtk.h>
-
-//this function makes the game board itself, creating the window
-void create_win_screen(int player){
-   
-    //creating a new window, requires new variables
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *label;
-
-    //creating the window using the init, but no arguments passed
-    gtk_init(NULL, NULL);
-   
-    //create game board window just like the main menu, same proccess
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Connect Four");
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-    gtk_window_maximize(GTK_WINDOW(window));
-
-    //this is the grid that is used to organize the board
-    grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    label = gtk_label_new(NULL);
-    if (player == 1) {
-        gtk_label_set_markup(GTK_LABEL(label), "<span font='60'>Player 1 wins!!</span>");
-    } else {
-        gtk_label_set_markup(GTK_LABEL(label), "<span font='60'>Player 2 wins!!</span>");
-    }
-    gtk_widget_set_halign(label, GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(label, GTK_ALIGN_START);
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
-
-    //showing the game board window
-    gtk_widget_show_all(window);
-
-    //running the gtk main loop
-    gtk_main();
->>>>>>> fd6c80a (added_win_screen_functionality)
 }
--- win_screen.h
+++ win_screen.h
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 void create_win_screen(int player);
-=======
-void create_win_screen(int player);
->>>>>>> fd6c80a (added_win_screen_functionality)
